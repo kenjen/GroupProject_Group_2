14a617fa552eac94467ba4d0a26b4ed43210ee7b
hannah <hmacallister@outlook.com>
2015-02-18 18:15:44 +0000

    moved FailureClassDAO into dao package

---
+++ GroupProject_Group2/src/main/java/com/project/dao/FailureClassDAO.java
@@ -0,0 +1,38 @@
+package com.project.dao;
+
+import java.util.List;
+
+import persistence.PersistenceUtil;
+
+import com.project.entities.FailureClass;
+
+public class FailureClassDAO 
+{
+	public static void main(String[] args) 
+	{
+		FailureClassDAO config = new FailureClassDAO();
+	}
+
+	public FailureClassDAO() 
+	{
+		createFailure(1, "Emergency");
+		viewFailure();
+	}
+
+	public void viewFailure() 
+	{
+		List<FailureClass> failures = PersistenceUtil.findAllFailures();
+		for (FailureClass f : failures) 
+		{
+			System.out.println("Failure " + f.getId() + " exists.");
+		}
+	}
+
+	public void createFailure(int failureClass, String desc) 
+	{
+		FailureClass failure = new FailureClass(failureClass, desc);
+		PersistenceUtil.persist(failure);
+		System.out.println("Failure created");
+	}
+
+}
