5bc6e646aea1d3432940fa89b713ac688b9ef4ae
Siobhain <SiobhainTopping@gmail.com>
2015-02-18 16:32:54 +0000

    test username setup

--- GroupProject_Group2/src/main/java/persistence/PersistenceUtil.java
+++ GroupProject_Group2/src/main/java/persistence/PersistenceUtil.java
@@ -13,7 +13,6 @@ import entity.FailureClass;
 
 public class PersistenceUtil implements Serializable 
 {
-	
 	private static final long serialVersionUID = 1L;
 
 	protected static EntityManagerFactory emf = 
