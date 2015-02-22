5d5424337fb2d7957461cda904e934406dc940cf
Kian <kianjennings@gmail.com>
2015-02-22 16:24:26 +0000

    Imsi lookup now returns EventCause objects instead of BaseData objects

--- GroupProject_Group2/src/main/java/com/project/rest/BaseDataRest.java
+++ GroupProject_Group2/src/main/java/com/project/rest/BaseDataRest.java
@@ -1,5 +1,6 @@
 package com.project.rest;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,10 +9,12 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.project.entities.BaseData;
+import com.project.entities.EventCause;
 import com.project.entities.FailureClass;
 import com.project.entities.UE;
 import com.project.service.BaseDataService;
@@ -58,6 +61,33 @@ public class BaseDataRest {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collection<FailureClass> getFailureClasses() {
 		return baseDataService.getFailureClasses();
+	}
+	
+	@GET
+	@Path("/eventcause")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<EventCause> getAllEventCause() {
+		 Collection<BaseData> base = baseDataService.getAllBaseData();
+		 List<EventCause> ec = new ArrayList<EventCause>();
+		 for(BaseData bd : base){
+			 ec.add(bd.getEventCauseFK());
+		 }
+		 
+		 return ec;
+	}
+	
+	@GET
+	@Path("/eventcause/{IMSI}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<EventCause> getAllEventCause(@PathParam("IMSI") Long imsi) {
+		 Collection<BaseData> base = baseDataService.getAllBaseData();
+		 List<EventCause> ec = new ArrayList<EventCause>();
+		 for(BaseData bd : base){
+			 if(bd.getImsi().equals(imsi)){
+				 ec.add(bd.getEventCauseFK());
+			 }
+		 }
+		 return ec;
 	}
 
 }
