2ce0924334802b7df1f101c8fbb522b7811e7e04
kian jennings <kianjennings@gmail.com>
2015-02-23 19:47:15 +0000

    removed truncate base_data_table when uploading

--- GroupProject_Group2/src/main/java/com/project/dao/JPABaseDataDAO.java
+++ GroupProject_Group2/src/main/java/com/project/dao/JPABaseDataDAO.java
@@ -38,8 +38,8 @@ public class JPABaseDataDAO implements BaseDataDAO {
 
 	@SuppressWarnings("rawtypes")
 	public void addAllBaseData(Collection baseDataList) {
-		entityManager.createNativeQuery("truncate table base_data").executeUpdate();
-		entityManager.createNativeQuery("alter table base_data AUTO_INCREMENT = 1").executeUpdate();
+		/*entityManager.createNativeQuery("truncate table base_data").executeUpdate();
+		entityManager.createNativeQuery("alter table base_data AUTO_INCREMENT = 1").executeUpdate();*/
 		for (Object o : baseDataList) {
 			entityManager.persist(o);
 		}
