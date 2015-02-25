9410ac5731ff4b46878ccd86f7a10b97fdcbfda5
kian jennings <kianjennings@gmail.com>
2015-02-25 09:43:08 +0000

    changed the way error_base_data is created now using BaseData.java instead of poi.Row

--- GroupProject_Group2/src/main/java/com/project/entities/ErrorBaseData.java
+++ GroupProject_Group2/src/main/java/com/project/entities/ErrorBaseData.java
@@ -1,8 +1,7 @@
 package com.project.entities;
 
 import java.io.Serializable;
-import java.util.Iterator;
-
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,9 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 
 @NamedQueries({ @NamedQuery(name = "ErrorBaseData.getAllErrorBaseData", query = "select e from ErrorBaseData e"), })
 @Entity
@@ -26,55 +22,41 @@ public class ErrorBaseData implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	private String date;
-	private String eventId;
-	private String failureClass;
+	private Date date;
+	private Integer eventId;
+	private Integer failureClass;
 	// UE Type
-	private String tac;
+	private Integer tac;
 	// Market
-	private String mnc;
+	private Integer mnc;
 	// Operator
-	private String mcc;
-	private String cellId;
-	private String duration;
-	private String causeCode;
+	private Integer mcc;
+	private Integer cellId;
+	private Integer duration;
+	private Integer causeCode;
 	private String neVersion;
-	private String imsi;
+	private Long imsi;
 	private String hier3Id;
 	private String hier32Id;
 	private String hier321Id;
 
 
-	public ErrorBaseData(Row row) {
-		Iterator<Cell> i = row.cellIterator();
-		Cell cell = i.next();
-		this.date = cell.toString();
-		cell = i.next();
-		this.eventId = cell.toString();
-		cell = i.next();
-		this.failureClass = cell.toString();
-		cell = i.next();
-		this.tac = cell.toString();
-		cell = i.next();
-		this.mnc = cell.toString();
-		cell = i.next();
-		this.mcc = cell.toString();
-		cell = i.next();
-		this.cellId = cell.toString();
-		cell = i.next();
-		this.duration = cell.toString();
-		cell = i.next();
-		this.causeCode = cell.toString();
-		cell = i.next();
-		this.neVersion = cell.toString();
-		cell = i.next();
-		this.imsi = cell.toString();
-		cell = i.next();
-		this.hier3Id = cell.toString();
-		cell = i.next();
-		this.hier32Id = cell.toString();
-		cell = i.next();
-		this.hier321Id = cell.toString();
+	public ErrorBaseData(BaseData baseDataRecord) {
+		super();
+		this.date = baseDataRecord.getDate();
+		this.eventId = baseDataRecord.getEventId();
+		this.failureClass = baseDataRecord.getFailureClass();
+		this.tac = baseDataRecord.getTac();
+		this.mnc = baseDataRecord.getMnc();
+		this.mcc = baseDataRecord.getMcc();
+		this.cellId = baseDataRecord.getCellId();
+		this.duration = baseDataRecord.getDuration();
+		this.causeCode = baseDataRecord.getCauseCode();
+		this.neVersion = baseDataRecord.getNeVersion();
+		this.imsi = baseDataRecord.getImsi();
+		this.hier3Id = baseDataRecord.getHier3Id();
+		this.hier32Id = baseDataRecord.getHier32Id();
+		this.hier321Id = baseDataRecord.getHier321Id();
 	}
 
 	public ErrorBaseData() {
@@ -92,83 +74,83 @@ public class ErrorBaseData implements Serializable {
 	}
 
 	@Column(name = "date")
-	public String getDate() {
+	public Date getDate() {
 		return date;
 	}
 
-	public void setDate(String date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 
 	@Column(name = "event_id")
-	public String getEventId() {
+	public Integer getEventId() {
 		return eventId;
 	}
 
-	public void setEventId(String eventId) {
+	public void setEventId(Integer eventId) {
 		this.eventId = eventId;
 	}
 
 	@Column(name = "failure_class")
-	public String getFailureClass() {
+	public Integer getFailureClass() {
 		return failureClass;
 	}
 
-	public void setFailureClass(String failureClass) {
+	public void setFailureClass(Integer failureClass) {
 		this.failureClass = failureClass;
 	}
 
 	@Column(name = "tac")
-	public String getTac() {
+	public Integer getTac() {
 		return tac;
 	}
 
-	public void setTac(String tac) {
+	public void setTac(Integer tac) {
 		this.tac = tac;
 	}
 
 	@Column(name = "mnc")
-	public String getMnc() {
+	public Integer getMnc() {
 		return mnc;
 	}
 
-	public void setMnc(String mnc) {
+	public void setMnc(Integer mnc) {
 		this.mnc = mnc;
 	}
 
 	@Column(name = "mcc")
-	public String getMcc() {
+	public Integer getMcc() {
 		return mcc;
 	}
 
-	public void setMcc(String mcc) {
+	public void setMcc(Integer mcc) {
 		this.mcc = mcc;
 	}
 
 	@Column(name = "cell_id")
-	public String getCellId() {
+	public Integer getCellId() {
 		return cellId;
 	}
 
-	public void setCellId(String cellId) {
+	public void setCellId(Integer cellId) {
 		this.cellId = cellId;
 	}
 
 	@Column(name = "duration")
-	public String getDuration() {
+	public Integer getDuration() {
 		return duration;
 	}
 
-	public void setDuration(String duration) {
+	public void setDuration(Integer duration) {
 		this.duration = duration;
 	}
 
 	@Column(name = "cause_code")
-	public String getCauseCode() {
+	public Integer getCauseCode() {
 		return causeCode;
 	}
 
-	public void setCauseCode(String causeCode) {
+	public void setCauseCode(Integer causeCode) {
 		this.causeCode = causeCode;
 	}
 
@@ -182,11 +164,11 @@ public class ErrorBaseData implements Serializable {
 	}
 
 	@Column(name = "imsi")
-	public String getImsi() {
+	public Long getImsi() {
 		return imsi;
 	}
 
-	public void setImsi(String imsi) {
+	public void setImsi(Long imsi) {
 		this.imsi = imsi;
 	}
 
--- GroupProject_Group2/src/main/java/com/project/reader/excel/ExcelBaseDataRead.java
+++ GroupProject_Group2/src/main/java/com/project/reader/excel/ExcelBaseDataRead.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Local;
@@ -66,66 +67,128 @@ public class ExcelBaseDataRead implements ReadBase {
 			Row row = hssfWorkBookSheet.getRow(currentRow);
 			boolean rowValid = true;
 			for (Cell cell : row) {
-				try{
-					if (cell.getColumnIndex() == 0) {
+				if (cell.getColumnIndex() == 0) {
+					try {
 						baseDataRecord.setDate(cell.getDateCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setDate(new Date(1l));
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 1) {
+				}
+				if (cell.getColumnIndex() == 1) {
+					try {
 						baseDataRecord.setEventId((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setEventId(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 2) {
+				}
+				if (cell.getColumnIndex() == 2) {
+					try {
 						baseDataRecord.setFailureClass((int) cell.getNumericCellValue());
-						
+					} catch (Exception e) {
+						baseDataRecord.setFailureClass(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 3) {
+				}
+				if (cell.getColumnIndex() == 3) {
+					try {
 						baseDataRecord.setTac((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setTac(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 4) {
+				}
+				if (cell.getColumnIndex() == 4) {
+					try {
 						baseDataRecord.setMcc((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setMcc(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 5) {
+				}
+				if (cell.getColumnIndex() == 5) {
+					try {
 						baseDataRecord.setMnc((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setMnc(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 6) {
+				}
+				if (cell.getColumnIndex() == 6) {
+					try {
 						baseDataRecord.setCellId((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setCellId(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 7) {
+				}
+				if (cell.getColumnIndex() == 7) {
+					try {
 						baseDataRecord.setDuration((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setDuration(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 8) {
+				}
+				if (cell.getColumnIndex() == 8) {
+					try {
 						baseDataRecord.setCauseCode((int) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setCauseCode(-1);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 9) {
+				}
+				if (cell.getColumnIndex() == 9) {
+					try {
 						baseDataRecord.setNeVersion(cell.getStringCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setNeVersion("INVALID");
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 10) {
+				}
+				if (cell.getColumnIndex() == 10) {
+					try {
 						baseDataRecord.setImsi((long) cell.getNumericCellValue());
+					} catch (Exception e) {
+						baseDataRecord.setImsi(-1l);
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 11) {
+				}
+				if (cell.getColumnIndex() == 11) {
+					try {
 						String cellVal = df.formatCellValue(cell);
 						baseDataRecord.setHier3Id(cellVal);
+					} catch (Exception e) {
+						baseDataRecord.setHier3Id("INVALID");
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 12) {
+				}
+				if (cell.getColumnIndex() == 12) {
+					try {
 						String cellVal = df.formatCellValue(cell);
 						baseDataRecord.setHier32Id(cellVal);
+					} catch (Exception e) {
+						baseDataRecord.setHier32Id("INVALID");
+						rowValid = false;
 					}
-					if (cell.getColumnIndex() == 13) {
+				}
+				if (cell.getColumnIndex() == 13) {
+					try {
 						String cellVal = df.formatCellValue(cell);
 						baseDataRecord.setHier321Id(cellVal);
+					} catch (Exception e) {
+						baseDataRecord.setHier321Id("INVALID");
+						rowValid = false;
 					}
-					
-				}catch(Exception e){
-					//catches all errors while parsing the data and marks the row as invalid
-					rowValid = false;
-					break;
-				}
+				}	
 			}
 			setAllLinks(baseDataRecord);
 			if(rowValid && validator.isValid(baseDataRecord)){
 				baseDatList.add(baseDataRecord);
 			}else{
 				invalidRowCount++;
-				errorBaseDatList.add(new ErrorBaseData(row));
+				errorBaseDatList.add(new ErrorBaseData(baseDataRecord));
 			}
 		}
 		hssfInputWorkbook.close();
