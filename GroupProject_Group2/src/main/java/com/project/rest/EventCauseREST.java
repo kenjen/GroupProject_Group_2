19b2ce3edea6603923d40a36f8c21f8886430e53
D14125320 <D14125320@BUS-1020-29.ict.ad.dit.ie>
2015-02-27 14:44:52 +0000

    changed EventCauseRest class

--- GroupProject_Group2/src/main/java/com/project/rest/EventCauseREST.java
+++ GroupProject_Group2/src/main/java/com/project/rest/EventCauseREST.java
@@ -1,27 +1,31 @@
 package com.project.rest;
 
 import java.util.Collection;
+
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.project.entities.EventCause;
 import com.project.service.EventCauseService;
 
-@Path("/base_data")
+@Path("/event_cause")
 public class EventCauseREST{
 	@EJB
 	private EventCauseService service;
 
 	@GET
-	@Path("/{IMSI}")
+	@Path("/{imsi}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Collection getEventCauseCombi(@PathParam("IMSI") Long imsi) {
+	public Collection getEventCauseCombi(@PathParam("imsi") Long imsi) {
 		Collection idsCombi = service.getFailuresIdsByIMSI(imsi);
 		return idsCombi;
 	}
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Collection getEventCause() {
