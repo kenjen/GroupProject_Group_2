cea79e21fe4ef913b417ce5587ad140c45ddcc46
kian jennings <kianjennings@gmail.com>
2015-03-09 15:26:53 +0000

    removed old class

--- GroupProject_Group2/src/main/java/com/project/fileupload/DirectoryWatcher.java
+++ GroupProject_Group2/src/main/java/com/project/fileupload/DirectoryWatcher.java
@@ -1,116 +1,86 @@
 package com.project.fileupload;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.Local;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.ejb.Asynchronous;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import persistence.PersistenceUtil;
-
+import com.project.dao.FileDAO;
 import com.project.entities.FileInfo;
+import com.project.service.FileService;
 
-@Local
-@Startup
-@Singleton
-public class DirectoryWatcher implements DirectoryWatcherInterface{
-	
-	//private instance to ensure only one Directory watcher in existence
-	private static DirectoryWatcher watcher;
-	
-	private static WatchService watchService;
-	
-	private static Thread t;
+@Stateless
+public class DirectoryWatcher {
 	
 	private static final Logger log = LoggerFactory.getLogger(DirectoryWatcher.class);
-
-	@PostConstruct
-	public void initialize(){
-		try {
-			watcher = createDirectoryWatcher();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	
-	public DirectoryWatcher(){
-		
-	}
+	@EJB
+	DirectoryWatcherTransactionInterface dirWatchTransaction;
 	
-	public static DirectoryWatcher createDirectoryWatcher() throws IOException{
-		// Folder we are going to watch
-		Path folder = Paths.get("/home/group2/");
-		
-		// Create a new Watch Service
-		watchService = FileSystems.getDefault().newWatchService();
-		log.info("DirectoryWatcher created");
-		// Register events
-		//folder.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+	boolean running = true;
+	
+	@Asynchronous
+	public void poll(String fileSystemPath) throws IOException {
+		/*Path folder = Paths.get(fileSystemPath);*/
+		Path folder = Paths.get("/upload/");
+		WatchService watchService = folder.getFileSystem().newWatchService();
 		folder.register(watchService, 
 				StandardWatchEventKinds.ENTRY_CREATE,
-				StandardWatchEventKinds.ENTRY_MODIFY,
+				/*StandardWatchEventKinds.ENTRY_MODIFY,*/
 				StandardWatchEventKinds.ENTRY_DELETE);
-		
-		t = new Thread(new Runnable() {
-			public void run() {
-				boolean running = true;
-				while(running){
-					try {
-						WatchKey watchKey = watchService.take();
-						List<WatchEvent<?>> events = watchKey.pollEvents();
-						for (WatchEvent event : events) {
-							if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
-								Path pathNewFile = (Path) event.context();
-								Path fileNameP = pathNewFile.getFileName();
-								String fileNameS = fileNameP.toFile().toString();
-								Path filePathP = pathNewFile.toAbsolutePath();
-								String filePathS = filePathP.toFile().toString();
-								FileInfo file = new FileInfo(fileNameS, filePathS);
-								log.info("file created: filename = " + fileNameS + "    filepath = " + filePathS);
-								PersistenceUtil.persist(file);
-							}
-						}
-					} catch (InterruptedException e) {
-						running = false;
-						e.printStackTrace();
+		while(running) {
+			WatchKey watchKey = null;
+			log.info("polling on " + folder.toAbsolutePath());
+			try {
+				watchKey = watchService.take();
+				
+				List<WatchEvent<?>> events = watchKey.pollEvents();
+				for (WatchEvent event : events) {
+					if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+						Path pathNewFile = (Path) event.context();
+						Path fileNameP = pathNewFile.getFileName();
+						String fileNameS = fileNameP.toFile().toString();
+						Path filePathP = pathNewFile.toAbsolutePath();
+						String filePathS = filePathP.toFile().toString();
+						FileInfo file = new FileInfo(fileNameS, filePathS);
+						log.info("file created: filename = " + fileNameS + "    filepath = " + filePathS);
+						
+						dirWatchTransaction.addFilePath(file);
+						
+						log.info("file persisted: filename = " + file.getFilename() + "    filepath = " + file.getFilepath());
+					}else if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+						Path filePath = (Path) event.context();
+						log.info("file deleted: " + filePath);
+					}else if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY) {
+						Path filePath = (Path) event.context();
+						log.info("file modified: " + filePath);
 					}
-					
+				}
+				//watchKey.reset();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return;
+			} finally {
+				if (watchKey != null) {
+					boolean valid = watchKey.reset();
+					log.info("***** key no longer valid, resetting *****");
+					if (!valid) break; // If the key is no longer valid, the directory is inaccessible so exit the loop.
 				}
 			}
-		});
-		t.start();
-		return watcher;
-	}
-	
-	public static DirectoryWatcher getDirectoryWatcher() throws IOException{
-		if(watcher==null){
-			watcher = createDirectoryWatcher();
 		}
-		return watcher;
-	}
-	
-	/*
-	 * return true if service was started
-	 * return false if already started
-	 */
-	
-	public static boolean startWatching() throws IOException{
-		if(watcher==null){
-			getDirectoryWatcher();
-			return true;
-		}
-		return false;
-	}
-
-	public static void stopWatching() throws IOException{
-		// Closes a watch service
-		watchService.close();
 	}
 }
