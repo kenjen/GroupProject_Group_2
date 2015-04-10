c3abf08d52e27e3f9715144f62b34023fc98209c
Kian <kianjennings@gmail.com>
2015-04-10 22:00:22 +0100

    fixed run on server not finding apache.poi

--- GroupProject_Group2/src/main/java/com/project/entities/FileInfo_.java
+++ GroupProject_Group2/src/main/java/com/project/entities/FileInfo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2015-04-09T09:21:25.140+0100")
+@Generated(value="Dali", date="2015-04-10T21:40:45.755+0100")
 @StaticMetamodel(FileInfo.class)
 public class FileInfo_ {
 	public static volatile SingularAttribute<FileInfo, String> filename;
