40c5ea0e0e432a4a7e2ef7852388e8745b276fbe
Kian <kianjennings@gmail.com>
2015-04-11 22:57:04 +0100

    fixed query errors due to % character, fixed errors after merging

--- GroupProject_Group2/src/main/java/com/project/entities/FileInfo_.java
+++ GroupProject_Group2/src/main/java/com/project/entities/FileInfo_.java
@@ -4,10 +4,10 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2015-04-11T12:22:42.717+0100")
+@Generated(value="Dali", date="2015-04-11T21:20:31.440+0100")
 @StaticMetamodel(FileInfo.class)
 public class FileInfo_ {
+	public static volatile SingularAttribute<FileInfo, Integer> id;
 	public static volatile SingularAttribute<FileInfo, String> filename;
 	public static volatile SingularAttribute<FileInfo, String> filepath;
-	public static volatile SingularAttribute<FileInfo, Integer> id;
 }
