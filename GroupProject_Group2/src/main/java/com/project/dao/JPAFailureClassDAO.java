ffecb8b7be05c695b5a2896860673abd83afbcfa
iant85 <ianthompson85@gmail.com>
2015-04-11 13:04:32 +0100

    Drilldown for query 11

--- GroupProject_Group2/src/main/java/com/project/dao/JPAFailureClassDAO.java
+++ GroupProject_Group2/src/main/java/com/project/dao/JPAFailureClassDAO.java
@@ -1,6 +1,7 @@
 package com.project.dao;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Local;
@@ -13,16 +14,29 @@ import com.project.entities.FailureClass;
 
 @Stateless
 @Local
-public class JPAFailureClassDAO implements FailureClassDAO{
+public class JPAFailureClassDAO implements FailureClassDAO {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	public Collection<FailureClass> getAllFailureClasses() {
-			Query query = em.createQuery("from FailureClass");
-			List<FailureClass> result = query.getResultList();
-			return result;
+		Query query = em.createQuery("from FailureClass");
+		List<FailureClass> result = query.getResultList();
+		return result;
 	}
 
-	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Object[]> specificFailuresForTopTenCombi(String country,
+			String operator, Integer cellId, Date startDate, Date endDate) {
+		Query query = em
+				.createNamedQuery("FailureClass.specificFailuresForTopTenCombi");
+		query.setParameter("country", country);
+		query.setParameter("operator", operator);
+		query.setParameter("cellId", cellId);
+		query.setParameter("startDate", startDate);
+		query.setParameter("endDate", endDate);
+		return query.getResultList();
+	}
+
 }
