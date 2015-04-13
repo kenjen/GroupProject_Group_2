d7d0784faee17fe1f9a39107b43187f19d9ffad0
MihaelaPatularu <patularumc@yahoo.com>
2015-04-13 15:54:57 +0100

    removed unused methods

--- GroupProject_Group2/src/main/java/com/project/entities/EventCause.java
+++ GroupProject_Group2/src/main/java/com/project/entities/EventCause.java
@@ -59,38 +59,6 @@ public class EventCause implements Serializable {
 		this.description = description;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((causeCode == null) ? 0 : causeCode.hashCode());
-		result = prime * result + ((eventId == null) ? 0 : eventId.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		EventCause other = (EventCause) obj;
-		if (causeCode == null) {
-			if (other.causeCode != null)
-				return false;
-		} else if (!causeCode.equals(other.causeCode))
-			return false;
-		if (eventId == null) {
-			if (other.eventId != null)
-				return false;
-		} else if (!eventId.equals(other.eventId))
-			return false;
-		return true;
-	}
-
 	public Integer getId() {
 		return id;
 	}
